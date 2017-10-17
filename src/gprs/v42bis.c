4f979bf4bae9ef18140f5be2acd0ee2751622b59
Max <msuraev@sysmocom.de>
2017-10-17 17:48:20 +0200

    Use new FSF address in license header
    The issue was uncovered by lintian while checking .deb packaging.
    
    Change-Id: Idfe0e3d61f2604521647e9ef5ffb984db96aabbe

--- src/gprs/v42bis.c
+++ src/gprs/v42bis.c
@@ -20,7 +20,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 /* THIS IS A WORK IN PROGRESS. IT IS NOT FINISHED. 
