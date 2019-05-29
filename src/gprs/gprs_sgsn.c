osmocom/osmo-sgsn

commit 176a4d2f33865a5c0433f8679f5e68f209d7b874

GTP: refactor the echo timer
Move the check of the echo timer into an own function.
The gtp echo timer must be re-check everytime the
echo-timer has been modified or deactivated via vty.

Fixes the TTCN3 SGSN_Tests.TC_attach_restart_ctr_echo

Change-Id: Ia33471a9a9cfc3887facb665c82094b99932052a

src/gprs/gprs_sgsn.c
@@ -489,6 +489,17 @@ void sgsn_pdp_ctx_free(struct sgsn_pdp_ctx *pdp)
 	talloc_free(pdp);
 }
 
+void sgsn_ggsn_ctx_check_echo_timer(struct sgsn_ggsn_ctx *ggc)
+{
+	if (llist_empty(&ggc->pdp_list) || ggc->echo_interval <= 0) {
+		if (osmo_timer_pending(&ggc->echo_timer))
+			osmo_timer_del(&ggc->echo_timer);
+	} else {
+		if (!osmo_timer_pending(&ggc->echo_timer))
+			osmo_timer_schedule(&ggc->echo_timer, ggc->echo_interval, 0);
+	}
+}
+
 /* GGSN contexts */
 static void echo_timer_cb(void *data)
 {
@@ -742,15 +753,14 @@ int sgsn_ggsn_ctx_drop_all_pdp_except(struct sgsn_ggsn_ctx *ggsn, struct sgsn_pd
 
 void sgsn_ggsn_ctx_add_pdp(struct sgsn_ggsn_ctx *ggc, struct sgsn_pdp_ctx *pdp)
 {
-	if (llist_empty(&ggc->pdp_list) && ggc->echo_interval > 0)
-		osmo_timer_schedule(&ggc->echo_timer, ggc->echo_interval, 0);
+	sgsn_ggsn_ctx_check_echo_timer(ggc);
+
 	llist_add(&pdp->ggsn_list, &ggc->pdp_list);
 }
 void sgsn_ggsn_ctx_remove_pdp(struct sgsn_ggsn_ctx *ggc, struct sgsn_pdp_ctx *pdp)
 {
 	llist_del(&pdp->ggsn_list);
-	if (llist_empty(&ggc->pdp_list) && osmo_timer_pending(&ggc->echo_timer))
-		osmo_timer_del(&ggc->echo_timer);
+	sgsn_ggsn_ctx_check_echo_timer(ggc);
 	if (pdp->destroy_ggsn)
 		sgsn_ggsn_ctx_free(pdp->ggsn);
 	pdp->ggsn = NULL;

include/osmocom/sgsn/gprs_sgsn.h
@@ -375,6 +375,7 @@ void sgsn_ggsn_ctx_drop_pdp(struct sgsn_pdp_ctx *pctx);
 int sgsn_ggsn_ctx_drop_all_pdp_except(struct sgsn_ggsn_ctx *ggsn, struct sgsn_pdp_ctx *except);
 void sgsn_ggsn_ctx_add_pdp(struct sgsn_ggsn_ctx *ggc, struct sgsn_pdp_ctx *pdp);
 void sgsn_ggsn_ctx_remove_pdp(struct sgsn_ggsn_ctx *ggc, struct sgsn_pdp_ctx *pdp);
+void sgsn_ggsn_ctx_check_echo_timer(struct sgsn_ggsn_ctx *ggc);
 
 struct apn_ctx {
 	struct llist_head list;

src/gprs/sgsn_vty.c
@@ -383,6 +383,7 @@ DEFUN(cfg_ggsn_echo_interval, cfg_ggsn_echo_interval_cmd,
 			     "not be lower than 60 seconds, use this value for " \
 			     "testing purposes only!%s", VTY_NEWLINE);
 
+	sgsn_ggsn_ctx_check_echo_timer(ggc);
 	return CMD_SUCCESS;
 }
 
@@ -395,6 +396,7 @@ DEFUN(cfg_ggsn_no_echo_interval, cfg_ggsn_no_echo_interval_cmd,
 	struct sgsn_ggsn_ctx *ggc = sgsn_ggsn_ctx_find_alloc(id);
 
 	ggc->echo_interval = -1;
+	sgsn_ggsn_ctx_check_echo_timer(ggc);
 
 	return CMD_SUCCESS;
 }

commit 1825ab5bae0b044bb6497200b5039284162d402e

sgsn: Fix echo timer not started upon first pdp ctx created
Commit 176a4d2f33865a5c0433f8679f5e68f209d7b874 moved echo timer related
code to its own function but did some mistakes when moving the logic
from several places into its own function. As a result, echo timer was
only enabled after the 2nd pdp ctx was created, instead of the expected
1st.
First, let's be consistent and always call the function *after* changing
state, since that's what the function expects. This fixes the issue.

Finally make the logic in the function more intuitive by checking in the
if clause the only case where actually the echo timer should be enabled:
Only if policy specifies so and we have at least 1 pdp ctx against that ggsn.

Fixes: 176a4d2f33865a5c0433f8679f5e68f209d7b874
Change-Id: I826030978edb61ea5a172c2b72f63758206a6246

src/gprs/gprs_sgsn.c
@@ -492,12 +492,15 @@ void sgsn_pdp_ctx_free(struct sgsn_pdp_ctx *pdp)
 
 void sgsn_ggsn_ctx_check_echo_timer(struct sgsn_ggsn_ctx *ggc)
 {
-	if (llist_empty(&ggc->pdp_list) || ggc->echo_interval <= 0) {
-		if (osmo_timer_pending(&ggc->echo_timer))
-			osmo_timer_del(&ggc->echo_timer);
-	} else {
-		if (!osmo_timer_pending(&ggc->echo_timer))
+	bool pending = osmo_timer_pending(&ggc->echo_timer);
+
+	/* Only enable if allowed by policy and at least 1 pdp ctx exists against ggsn */
+	if (!llist_empty(&ggc->pdp_list) && ggc->echo_interval > 0) {
+		if (!pending)
 			osmo_timer_schedule(&ggc->echo_timer, ggc->echo_interval, 0);
+	} else {
+		if (pending)
+			osmo_timer_del(&ggc->echo_timer);
 	}
 }
 
@@ -754,9 +757,8 @@ int sgsn_ggsn_ctx_drop_all_pdp_except(struct sgsn_ggsn_ctx *ggsn, struct sgsn_pd
 
 void sgsn_ggsn_ctx_add_pdp(struct sgsn_ggsn_ctx *ggc, struct sgsn_pdp_ctx *pdp)
 {
-	sgsn_ggsn_ctx_check_echo_timer(ggc);
-
 	llist_add(&pdp->ggsn_list, &ggc->pdp_list);
+	sgsn_ggsn_ctx_check_echo_timer(ggc);
 }
 void sgsn_ggsn_ctx_remove_pdp(struct sgsn_ggsn_ctx *ggc, struct sgsn_pdp_ctx *pdp)
 {