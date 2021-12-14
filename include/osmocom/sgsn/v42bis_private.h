57425a38051d4a3b098eb9583e9569cfc8b98c30
Oliver Smith <osmith@sysmocom.de>
2021-12-14 12:21:06 +0100

    treewide: remove FSF address
    Remove the paragraph about writing to the Free Software Foundation's
    mailing address. The FSF has changed addresses in the past, and may do
    so again. In 2021 this is not useful, let's rather have a bit less
    boilerplate at the start of source files.
    
    Change-Id: I9bc57a7152015e0f37e3d1573921d6d5d3c0449b

--- include/osmocom/sgsn/v42bis_private.h
+++ include/osmocom/sgsn/v42bis_private.h
@@ -17,10 +17,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #if !defined(_SPANDSP_PRIVATE_V42BIS_H_)
