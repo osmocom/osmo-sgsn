7dc90aa32b3c3057e86e898755ce096bfc8fe40c
Alexander Couzens <lynxis@fe80.eu>
2024-08-16 15:12:19 +0200

    move gsm48_gmm_att_tlvdef into gprs_gmm_util
    The gprs_gmm.c is huge. Move message parsing into gprS_gmm_util.c.
    
    Change-Id: I67dcdb986fd01dc093501d324b5c376246a5d30d

---
+++ include/osmocom/sgsn/gprs_gmm_util.h
@@ -0,0 +1,10 @@
+#ifndef _GPRS_GMM_UTIL_H
+#define _GPRS_GMM_UTIL_H
+
+#include <osmocom/gsm/tlv.h>
+
+struct msgb;
+
+extern const struct tlv_definition gsm48_gmm_ie_tlvdef;
+
+#endif /* _GPRS_GMM_UTIL_H */
