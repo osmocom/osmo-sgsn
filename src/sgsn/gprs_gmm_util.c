0b18d6bf29688a5a9da829a94242ff0487000a5e
Alexander Couzens <lynxis@fe80.eu>
2024-12-01 23:11:59 +0100

    fixup TLV parsing in gmm_util
    Change-Id: I1b55de1f49dc7f6374f7d1480c1fe96982badf70

--- src/sgsn/gprs_gmm_util.c
+++ src/sgsn/gprs_gmm_util.c
@@ -137,7 +137,7 @@ int gprs_gmm_parse_att_req(struct msgb *msg, struct gprs_gmm_att_req *att_req)
 	ret = tlv_parse(&att_req->tlv, &gsm48_gmm_ie_tlvdef,
 			cur, msgb_l3len(msg) - mandatory_fields_len, 0, 0);
 
-	if (ret < 0) {
+	if (ret < 0 && !OSMO_TLVP_ERR_UNKNOWN_TLV_TYPE) {
 		LOGP(DMM, LOGL_NOTICE, "%s(): tlv_parse() failed (%d)\n", __func__, ret);
 		/* gracefully handle unknown IEs (partial parsing) */
 		if (ret != OSMO_TLVP_ERR_UNKNOWN_TLV_TYPE)
@@ -199,7 +199,7 @@ int gprs_gmm_parse_ra_upd_req(struct msgb *msg, struct gprs_gmm_ra_upd_req *rau_
 
 	ret = tlv_parse(&rau_req->tlv, &gsm48_gmm_ie_tlvdef,
 			cur, msgb_l3len(msg) - mandatory_fields_len, 0, 0);
-	if (ret < 0) {
+	if (ret < 0 && !OSMO_TLVP_ERR_UNKNOWN_TLV_TYPE) {
 		LOGP(DMM, LOGL_NOTICE, "%s(): tlv_parse() failed (%d)\n", __func__, ret);
 		/* gracefully handle unknown IEs (partial parsing) */
 		if (ret != OSMO_TLVP_ERR_UNKNOWN_TLV_TYPE)
