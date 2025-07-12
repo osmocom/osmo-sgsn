7110be7a9c040af19eda45b3fa0430f324a135ca
Alexander Couzens <lynxis@fe80.eu>
2025-07-12 14:10:48 +0200

    routing area: add LOGRA() logging macro
    Prepend the routing area in the log message.
    
    Change-Id: Ib287b70c34b21ce64bc864f69fcf6af5fa0698b9

--- include/osmocom/sgsn/gprs_routing_area.h
+++ include/osmocom/sgsn/gprs_routing_area.h
@@ -9,6 +9,18 @@
 #include <osmocom/core/utils.h>
 #include <osmocom/gsm/gsm23003.h>
 
+/* rai -> struct osmo_routing_area_id * */
+#define LOGRAI(level, rai, fmt, args...) \
+	do {\
+		char __log_rai_buf[32]; \
+		osmo_rai_name2_buf(__log_rai_buf, sizeof(__log_rai_buf), rai); \
+		LOGP(DRA, level, "RA(%s) " fmt, __log_rai_buf, ## args); \
+	} while (0)
+
+/* ra -> struct sgsn_ra * */
+#define LOGRA(level, ra, fmt, args...) \
+	LOGRAI(level, (&(ra)->rai), fmt, ## args)
+
 struct sgsn_instance;
 struct sgsn_mm_ctx;
 
