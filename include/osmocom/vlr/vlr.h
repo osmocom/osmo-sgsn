290290cd5b3a169c6dc73db0a56c5e69953c9394
Alexander Couzens <lynxis@fe80.eu>
2026-08-20 18:13:11 +0200

    Revert "diff towards msc - to be removed"
    This reverts commit a07fc9f1a38d7db76dc8b1c6ca0494258a4732af.

--- include/osmocom/vlr/vlr.h
+++ include/osmocom/vlr/vlr.h
@@ -13,7 +13,6 @@
 #include <osmocom/gsm/gsm0808.h>
 #include <osmocom/gsm/gsup.h>
 #include <osmocom/gsm/ipa.h>
-#include <osmocom/msc/msc_common.h>
 #include <osmocom/gsupclient/gsup_client.h>
 #include <osmocom/vlr/vlr_sgs.h>
 
