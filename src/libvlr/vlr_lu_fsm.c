79833fada0cb94427a898b3bd8a471c194722354
Alexander Couzens <lynxis@fe80.eu>
2026-08-31 00:29:16 +0200

    delay vlr ra start
    Change-Id: Id525429eefaf46aac9b715ac091e9c010e2e50c3

--- src/libvlr/vlr_lu_fsm.c
+++ src/libvlr/vlr_lu_fsm.c
@@ -995,10 +995,13 @@ static void vlr_loc_upd_node1(struct osmo_fsm_inst *fi)
 
 	OSMO_ASSERT(vsub);
 
+	/* check if we need to do an authentication, if only ciphering, check if ciphering key is available */
 	if (try_auth(lfp)) {
 		/* Authenticate_VLR */
 		osmo_fsm_inst_state_chg(fi, VLR_ULA_S_WAIT_AUTH,
 					LU_TIMEOUT_LONG, 0);
+		/* if auth_fsm_start fails and the fsm already ran into a failure of auth_fsm,.
+		 * auth_fsm is assigned an already free'ed fsm */
 		vsub->auth_fsm = auth_fsm_create(lfp->vsub,
 						fi,
 						VLR_ULA_E_AUTH_SUCCESS,
